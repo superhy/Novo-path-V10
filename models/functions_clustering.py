0ac60c92f74bf1c02a837ba3d28165780de752bd
superhy <yang.hu@ndm.ox.ac.uk>
2022-11-10 16:11:02 +0000

    class for clustering, 2022-11-09

--- models/functions_clustering.py
+++ models/functions_clustering.py
@@ -2,13 +2,10 @@
 @author: Yang Hu
 '''
 
-'''
-clustering algorithm functions
-'''
-
 from sklearn.cluster._affinity_propagation import AffinityPropagation
+from sklearn.cluster._dbscan import DBSCAN
 from sklearn.cluster._kmeans import KMeans
-from sklearn.cluster._mean_shift import MeanShift
+from sklearn.cluster._mean_shift import MeanShift, estimate_bandwidth
 from sklearn.cluster._spectral import SpectralClustering
 import torch
 from torch.nn.functional import softmax
@@ -17,151 +14,175 @@ import numpy as np
 from support.tools import Time
 
 
-def clustering_tiles_K_means(attK_input_richencodes_tuples, n_clusters=10):
-    '''
-    running a k-means clustering on tiles' encodes
-    
-    Args:
-        attK_input_richencodes_tuples: EMT-high or/and low rich encodes tuple list
-        
-    Return:
-        cluster_res_info_pkg: the cluster results list of tuple
-            (cluster result (int), encode (vector), tile (tile object), slide_id (string))
-    '''
-    
-    cluster_alg_name = '{}@Kmeans'.format(n_clusters)
-    
-    clustering_time = Time()
-    
-    encodes, tiles, slide_ids = [], [], []
-    for info_tuple in attK_input_richencodes_tuples:
-        encodes.append(info_tuple[0])
-        tiles.append(info_tuple[1])
-        slide_ids.append(info_tuple[2])
-    
-    encodes_X = np.array(encodes)
-    
-    clustering = KMeans(n_clusters=n_clusters)
-    cluster_res = clustering.fit_predict(encodes_X)
-    
-    # combine the results package
-    cluster_res_info_pkg = []
-    for i, res in enumerate(cluster_res):
-        cluster_res_info_pkg.append((res, encodes[i], tiles[i], slide_ids[i]))
-        
-    print('execute KMeans clustering on %d tiles with time: %s sec' % (len(attK_input_richencodes_tuples), str(clustering_time.elapsed())))
-    
-    return cluster_res_info_pkg, cluster_alg_name, clustering.cluster_centers_
 
-def clustering_tiles_MeanShift(attK_input_richencodes_tuples):
-    '''
-    running mean-shift clustering on tiles' encodes
-    
-    Args:
-        attK_input_richencodes_tuples: EMT-high or/and low rich encodes tuple list
-        
-    Return:
-        cluster_res_info_pkg: the cluster results list of tuple
-            (cluster result (int), encode (vector), tile (tile object), slide_id (string))
-    '''
-    cluster_alg_name = 'n@MeanShift'
-    
-    clustering_time = Time()
-    
-    encodes, tiles, slide_ids = [], [], []
-    for info_tuple in attK_input_richencodes_tuples:
-        encodes.append(info_tuple[0])
-        tiles.append(info_tuple[1])
-        slide_ids.append(info_tuple[2])
-    
-    encodes_X = np.array(encodes)
-    
-    clustering = MeanShift()
-    cluster_res = clustering.fit_predict(encodes_X)
-    
-    # combine the results package
-    cluster_res_info_pkg = []
-    for i, res in enumerate(cluster_res):
-        cluster_res_info_pkg.append((res, encodes[i], tiles[i], slide_ids[i]))
-        
-    print('execute MeanShift clustering on %d tiles with time: %s sec' % (len(attK_input_richencodes_tuples), str(clustering_time.elapsed())))
-    
-    return cluster_res_info_pkg, cluster_alg_name, clustering.cluster_centers_
+def load_tiles_encode_rich_tuples():
+    tiles_richencode_tuples = []
+    return tiles_richencode_tuples
 
-def clustering_tiles_AffinityPropagation(attK_bi_richencodes_tuples):
-    '''
-    running a graph-based clustering method named "AffinityPropagation" on tiles' encodes
-    this cluster do not need to set the cluster number
-    
-    Args:
-        attK_bi_richencodes_tuples: EMT-high or low (only available for one side) rich encodes tuple list
-        
-    Return:
-        cluster_res_info_pkg: the cluster results list of tuple
-            (cluster result (int), encode (vector), tile (tile object), slide_id (string))
-    '''
-    
-    cluster_alg_name = 'n@AffinityPropa'
-    
-    clustering_time = Time()
-    
-    encodes, tiles, slide_ids = [], [], []
-    for info_tuple in attK_bi_richencodes_tuples:
-        encodes.append(info_tuple[0])
-        tiles.append(info_tuple[1])
-        slide_ids.append(info_tuple[2])
-    
-    encodes_X = np.array(encodes)
-    
-    clustering = AffinityPropagation(damping=0.5, max_iter=200, random_state=0)
-    cluster_res = clustering.fit_predict(encodes_X)
-    
-    # combine the results package
-    cluster_res_info_pkg = []
-    for i, res in enumerate(cluster_res):
-        cluster_res_info_pkg.append((res, encodes[i], tiles[i], slide_ids[i]))
-        
-    print('execute AffinityPropagation clustering on %d tiles with time: %s sec' % (len(attK_bi_richencodes_tuples), str(clustering_time.elapsed())))
-    
-    return cluster_res_info_pkg, cluster_alg_name, clustering.cluster_centers_
+def load_tiles_semantic_rich_tuples():
+    tiles_richencode_tuples = []
+    return tiles_richencode_tuples
 
+def load_tiles_graph_embed_rich_tuples():
+    tiles_richencode_tuples = []
+    return tiles_richencode_tuples
 
-def clustering_tiles_SpectralClustering(attK_bi_richencodes_tuples, n_clusters=10):
+class Instance_Clustering():
     '''
-    running a SpectralClustering clustering on tiles' encodes
-    this one should be set as the default
-    
-    Args:
-        attK_bi_richencodes_tuples: EMT-high or low (only available for one side) rich encodes tuple list
-        
-    Return:
-        cluster_res_info_pkg: the cluster results list of tuple
-            (cluster result (int), encode (vector), tile (tile object), slide_id (string))
+    Clustering for tile instances from all (multiple) slides
     '''
-    
-    cluster_alg_name = '{}@SpectralCluster'.format(n_clusters)
-    
-    clustering_time = Time()
-    
-    encodes, tiles, slide_ids = [], [], []
-    for info_tuple in attK_bi_richencodes_tuples:
-        encodes.append(info_tuple[0])
-        tiles.append(info_tuple[1])
-        slide_ids.append(info_tuple[2])
-    
-    encodes_X = np.array(encodes)
-    
-    clustering = SpectralClustering(n_clusters=n_clusters)
-    cluster_res = clustering.fit_predict(encodes_X)
-    
-    # combine the results package
-    cluster_res_info_pkg = []
-    for i, res in enumerate(cluster_res):
-        cluster_res_info_pkg.append((res, encodes[i], tiles[i], slide_ids[i]))
+    def __init__(self, ENV_task, encoder, cluster_name, embed_type='encode'):
+        '''
+        Args:
+            ENV_task: task environment (hyper-parameters)
+            encoder: used for load the embedding of tile-features (in default will be a ViT model)
+            cluster_name: the name of selected clustering algorithm
+                'Kmeans', 'Spectral', 'MeanShift', 'AffinityPropa', 'DBSCAN'
+            embed_type: the type of the embedding of tile features
+                'encode' -> the typical encoding from encoder (available for both ResNet and ViT)
+                'semantic' ->  the semantic map extracted from ViT encoder
+                'graph' -> the topological graph embeded from ViT encoder
+        '''
         
-    print('execute SpectralClustering clustering on %d tiles with time: %s sec' % (len(attK_bi_richencodes_tuples), str(clustering_time.elapsed())))
+        self.ENV_task = ENV_task
+        ''' prepare some parames '''
+        _env_task_name = self.ENV_task.TASK_NAME
+        
+        self.model_store_dir = self.ENV_task.MODEL_FOLDER
+        self.alg_name = 'cluster-{}_{}'.format(cluster_name, _env_task_name)
+        self.encoder = encoder
+        self.cluster_name = cluster_name
+        
+        print('![Initial Stage] clustering mode')
+        print('Initialising the embedding of overall tile features...')
+        
+        self.tiles_richencode_tuples = []
+        
     
-    return cluster_res_info_pkg, cluster_alg_name, clustering.cluster_centers_
+    def fit_predict(self):
+        '''
+        fit the clustering algorithm and get the predict results
+        
+        Return:
+            clustering_res_pkg: list of results tuple: [(cluster_res, encode, tile, slide_id), ()...]
+            clustering.cluster_centers_: clustering centres
+        '''
+        encodes, tiles, slide_ids = [], [], []
+        for info_tuple in self.tiles_richencode_tuples:
+            encodes.append(info_tuple[0])
+            tiles.append(info_tuple[1])
+            slide_ids.append(info_tuple[2])
+        encodes_X = np.array(encodes)
+        print('data tuples loaded!')
+        
+        print('Using clustering: {}'.format(self.cluster_name))
+        if self.cluster_name == 'Kmeans':
+            clustering = self.load_K_means()
+        elif self.cluster_name == 'Spectral':
+            clustering = self.load_SpectralClustering()
+        elif self.cluster_name == 'MeanShift':
+            clustering = self.load_MeanShift(encodes_X)
+        elif self.cluster_name == 'AffinityPropa':
+            clustering = self.load_AffinityPropagation()
+        elif self.cluster_name == 'DBSCAN':
+            clustering = self.load_DBSCAN()
+        else:
+            clustering = self.load_K_means()
+        
+        clustering_time = Time()
+        print('In fitting...', end='')
+        cluster_res = clustering.fit_predict(encodes_X)
+        # combine the results package
+        clustering_res_pkg = []
+        for i, res in enumerate(cluster_res):
+            clustering_res_pkg.append((res, encodes[i], tiles[i], slide_ids[i]))
+        print('execute %s clustering on %d tiles with time: %s sec' % (self.cluster_name,
+                                                                       len(self.tiles_richencode_tuples),
+                                                                       str(clustering_time.elapsed())))
+        
+        return clustering_res_pkg, clustering.cluster_centers_
+        
+        
+    def load_K_means(self):
+        '''
+        load the model of k-means clustering algorithm
+            with number of clusters
+        
+        Return:
+            clustering: empty clustering model without fit
+        '''
+        n_clusters = 10
+        
+        clustering = KMeans(n_clusters=n_clusters)
+        return clustering
+    
+    def load_SpectralClustering(self):
+        '''
+        load the model of spectral clustering algorithm
+            cite: Normalized cuts and image segmentation, 2000 Jianbo Shi, Jitendra Malik
+            with number of clusters
+            
+        Return:
+            clustering: empty clustering model without fit
+        '''
+        n_clusters = 10
+        assign_labels = 'discretize'
+        
+        clustering = SpectralClustering(n_clusters=n_clusters, assign_labels=assign_labels)
+        return clustering
+        
+    def load_MeanShift(self, X):
+        '''
+        load the model of meanshift clustering algorithm
+            Dorin Comaniciu and Peter Meer, “Mean Shift: A robust approach toward feature space analysis”.
+                IEEE Transactions on Pattern Analysis and Machine Intelligence. 2002. pp. 603-619.
+            without number of clusters
+        
+        Args:
+            X: we need the input encodes_X here
+        
+        Return:
+            clustering: empty clustering model without fit
+        '''
+        bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+        bin_seeding = True
+        
+        clustering = MeanShift(bandwidth=bandwidth, bin_seeding=bin_seeding)
+        return clustering
+    
+    def load_AffinityPropagation(self):
+        '''
+        load the model of affinity propagation clustering algorithm
+            without number of clusters
+        
+        Return:
+            clustering: empty clustering model without fit
+        '''
+        damping = 0.5
+        max_iter = 200
+        random_state = 0
+        linkage = 'ward'
+        
+        clustering = AffinityPropagation(damping=damping, max_iter=max_iter, 
+                                         linkage = linkage,random_state=random_state)
+        return clustering
+    
+    def load_DBSCAN(self):
+        '''
+        load the model of DBSCAN clustering algorithm
+            Ester, M., H. P. Kriegel, J. Sander, and X. Xu, “A Density-Based Algorithm for Discovering Clusters in Large Spatial Databases with Noise”.
+                In: Proceedings of the 2nd International Conference on Knowledge Discovery and Data Mining, Portland, OR, AAAI Press, pp. 226-231. 1996
+            without number of clusters
+            
+        Return:
+            clustering: empty clustering model without fit
+        '''
+        eps = 0.3
+        min_samples = 10
+        
+        clustering = DBSCAN(eps=eps, min_samples=min_samples)
+        return clustering
+    
 
 if __name__ == '__main__':
     pass
