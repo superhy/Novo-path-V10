ad15dff95323a4513e7095815081b978dfb76b47
superhy <yang.hu@ndm.ox.ac.uk>
2022-11-26 23:08:14 +0000

    debug for borad tiles 2022-11-26

--- models/functions_vit_ext.py
+++ models/functions_vit_ext.py
@@ -74,8 +74,9 @@ def avg_neigb_encodes(tiles_en_nd, tile_loc_dict, key_encode_tuple):
     
     combine_encodes = [encode]
     for n_k in neigb_keys:
-        encode_idx = tile_loc_dict[n_k][0]
-        combine_encodes.append(tiles_en_nd[encode_idx])
+        if n_k in tile_loc_dict.keys():
+            encode_idx = tile_loc_dict[n_k][0]
+            combine_encodes.append(tiles_en_nd[encode_idx])
     combine_encodes_nd = np.array(combine_encodes)
     combine_encodes_nd = np.average(combine_encodes_nd, axis=1)
     
