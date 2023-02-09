21bda53f45c14c53293c2841dcd702bca6fdfddb
superhy <yang.hu@ndm.ox.ac.uk>
2023-02-09 17:24:35 +0000

    color map

--- test.py
+++ test.py
@@ -193,6 +193,41 @@ def test_neb_nx_graph():
     # node_list.pop(1)
     # print(node_list, canvas_nxG.nodes())
         
+        
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
+cmaps = {}
+gradient = np.linspace(0, 1, 256)
+gradient = np.vstack((gradient, gradient))
+
+def plot_color_gradients(category, cmap_list):
+    # Create figure and adjust figure height to number of colormaps
+    nrows = len(cmap_list)
+    figh = 0.35 + 0.15 + (nrows + (nrows - 1) * 0.1) * 0.22
+    fig, axs = plt.subplots(nrows=nrows + 1, figsize=(6.4, figh), dpi=100)
+    fig.subplots_adjust(top=1 - 0.35 / figh, bottom=0.15 / figh,
+                        left=0.2, right=0.99)
+    axs[0].set_title(f'{category} colormaps', fontsize=14)
+
+    for ax, name in zip(axs, cmap_list):
+        ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(name))
+        ax.text(-0.01, 0.5, name, va='center', ha='right', fontsize=10,
+                transform=ax.transAxes)
+
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axs:
+        ax.set_axis_off()
+
+    # Save colormap list for later.
+    cmaps[category] = cmap_list
+    
+    plt.show()
+    
+
+
+
     
 if __name__ == '__main__':
 #     test_filter_slide_img() # 1
@@ -201,7 +236,9 @@ if __name__ == '__main__':
 
 #     test_numpy()
     # test_nx_graph()
-    test_neb_nx_graph()
+    # test_neb_nx_graph()
+    
+    plot_color_gradients('Qualitative', ['tab10'])
 
 
 
