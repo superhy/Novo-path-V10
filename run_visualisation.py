664cf387c9d66cc421c3317adf9cfafe7943d79e
superhy <yang.hu@ndm.ox.ac.uk>
2023-04-05 18:19:48 +0100

    some fix

--- run_visualisation.py
+++ run_visualisation.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
 #     ENV_task = env_flinc_he.ENV_FLINC_HE_STEA_C2
 #     ENV_task = env_flinc_psr.ENV_FLINC_PSR_FIB_C3
 
-    task_ids = [20, 21, 22, 29]
+    # task_ids = [20, 21, 22, 29]
     # task_ids = [61, 62]
-    # task_ids = [29]
+    task_ids = [20, 21]
     
     if 10 in task_ids:
         vit_model_filename = 'checkpoint_ViT-6-8-PT-Dino_unsupervised[250]2022-11-02.pth'
@@ -41,12 +41,14 @@ if __name__ == '__main__':
     if 20 in task_ids:
         # clustering_pkl_name = 'clst-res_Kmeans-encode_unsupervised2023-03-02.pkl'
         # clustering_pkl_name = 'clst-res_Kmeans-neb_encode_unsupervised2023-03-02.pkl' # clst-6
-        clustering_pkl_name = 'clst-res_Kmeans-neb_encode_unsupervised2023-03-03.pkl' # clst-10
+        # clustering_pkl_name = 'clst-res_Kmeans-neb_encode_unsupervised2023-03-03.pkl' # clst-10
+        clustering_pkl_name = 'clst-res_Kmeans-region_ctx_unsupervised2023-04-05.pkl' # clst-6 reg
         _run_make_clsuters_space_maps(ENV_task, clustering_pkl_name, nb_picked=5000)
     if 21 in task_ids:
         # clustering_pkl_name = 'clst-res_Kmeans-encode_unsupervised2023-03-02.pkl'
         # clustering_pkl_name = 'clst-res_Kmeans-neb_encode_unsupervised2023-03-02.pkl' # clst-6
-        clustering_pkl_name = 'clst-res_Kmeans-neb_encode_unsupervised2023-03-03.pkl' # clst-10
+        # clustering_pkl_name = 'clst-res_Kmeans-neb_encode_unsupervised2023-03-03.pkl' # clst-10
+        clustering_pkl_name = 'clst-res_Kmeans-region_ctx_unsupervised2023-04-05.pkl' # clst-6 reg
         _run_make_spatial_clusters_on_slides(ENV_task, clustering_pkl_name, keep_org_slide=False)
     if 22 in task_ids:
         # clustering_pkl_name = 'clst-res_Kmeans-encode_unsupervised2023-03-02.pkl'
