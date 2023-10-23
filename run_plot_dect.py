340e1ec339e8c3025fe6a16d7935bef23a224333
superhy <yang.hu@ndm.ox.ac.uk>
2023-10-24 00:51:05 +0100

    best-5-from-10 visualization

--- run_plot_dect.py
+++ run_plot_dect.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     if 0 in task_ids:
         pass
     if 1.1 in task_ids:
-        heatmap_pkl_name = 'topK_map_GatedAttPool-g_Pool-0_ballooning_score_bi_[80]2023-10-20.pkl'
+        heatmap_pkl_name = 'topK_map_GatedAttPool-g_Pool-1_ballooning_score_bi_[114]2023-10-20.pkl'
         ENV_annotation = env_flinc_p62.ENV_FLINC_P62_BALL_BI
         
         _plot_topK_attention_heatmaps(ENV_task, ENV_annotation, heatmap_pkl_name)
